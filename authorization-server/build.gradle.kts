c1e1b46b90999a9c4e4b0eb210e07c5f1a278afb
MaksimKavalenka <25153828+MaksimKavalenka@users.noreply.github.com>
2024-02-29 00:21:36 +0300

    Authorization Server

---
+++ authorization-server/build.gradle.kts
@@ -0,0 +1,81 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
+
+plugins {
+    id("java")
+    id("maven-publish")
+    id("org.springframework.boot") version "3.1.4"
+}
+
+apply(plugin = "io.spring.dependency-management")
+apply(plugin = "maven-publish")
+
+configurations {
+    listOf(apiElements, runtimeElements).forEach {
+        val jar by tasks
+        it.get().outgoing.artifacts.removeIf { it.buildDependencies.getDependencies(null).contains(jar) }
+        it.get().outgoing.artifact(tasks.bootJar)
+    }
+}
+
+repositories {
+    mavenCentral()
+
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/MaksimKavalenka/microservices-learning")
+        credentials {
+            username = project.findProperty("gpr.user") as String
+            password = project.findProperty("gpr.key") as String
+        }
+    }
+}
+
+configure<PublishingExtension> {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/MaksimKavalenka/microservices-learning")
+            credentials {
+                username = project.findProperty("gpr.user") as String
+                password = project.findProperty("gpr.key") as String
+            }
+        }
+    }
+
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+        }
+    }
+}
+
+the<DependencyManagementExtension>().apply {
+    imports {
+        mavenBom(BOM_COORDINATES)
+    }
+}
+
+dependencies {
+    annotationProcessor("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok")
+
+    implementation("org.slf4j:slf4j-api")
+
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-authorization-server")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
+    archiveClassifier.set("")
+}
